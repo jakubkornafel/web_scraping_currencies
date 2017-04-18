7d0bb45b07193df8710aed7ebc38d34375621899
jakubkornafel <qba108@gmail.com>
2017-04-18 15:36:10 +0200

    Add coments on installing pandas

--- currency_scraper.py
+++ currency_scraper.py
@@ -1,3 +1,6 @@
+# You will need to install "pandas" package in your virtual environment by using: pip install pandas
+
+
 import pandas as pd
 
 source = pd.read_html('https://cinkciarz.pl/kantor/kursy-walut-cinkciarz-pl')
